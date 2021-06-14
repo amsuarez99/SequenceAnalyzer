ba822fb31173aa1beb7b6ec90571432170d87df1
Adrián Marcelo Suárez Ponce <amsuarez99@gmail.com>
2021-06-14 05:01:57 -0500

    Reads from file and stores in server

--- client.c
+++ client.c
@@ -7,6 +7,7 @@
 #define SIZE 1024
 #define FILECHUNK 4096
 #define DEBUG 1
+#define PORT 8000
 
 void *getInput() {
     char *input = (char *) malloc(sizeof(char) * SIZE);
@@ -28,12 +29,6 @@ void sendFile(int sockfd, int fd) {
     }
 }
 
-void sendFile(int sockfd, int fd) {
-    char buff[FILECHUNK];
-    int cRead;
-    while()
-}
-
 char menu() {
     char op;
     printf("===MAIN MENU===\n");
@@ -46,16 +41,37 @@ char menu() {
     return op;
 }
 
+int getFileSize(FILE *fp) {
+    int fSize;
+    // Count seq file length
+    fseek(fp, 0, SEEK_END);
+    fSize = ftell(fp);
+    rewind(fp);
+    return fSize;
+}
+
+int SocketReceive(int hSocket, char *Rsp, int RvcSize)
+{
+    int shortRetval = -1;
+    struct timeval tv;
+    tv.tv_sec = 20; /* 20 Secs Timeout */
+    tv.tv_usec = 0;
+    if (setsockopt(hSocket, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(tv)) < 0)
+    {
+        perror("Time out...\n");
+        exit(EXIT_FAILURE);
+    }
+    shortRetval = recv(hSocket, Rsp, RvcSize, 0);
+    return shortRetval;
+}
+
 int main()
 {
     char *ip = "127.0.0.1";
-    int port = 8080;
     int e;
 
     int sockfd;
     struct sockaddr_in server_addr;
-    FILE *fp;
-    char *filename = "reference.txt";
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -66,7 +82,7 @@ int main()
     printf("[+]Server socket created successfully.\n");
 
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = port;
+    server_addr.sin_port = PORT;
     server_addr.sin_addr.s_addr = inet_addr(ip);
 
     e = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
@@ -77,31 +93,71 @@ int main()
     }
     printf("[+]Connected to Server.\n");
 
-    char op;
-    int fd;
-    char message[SIZE] = {0};
+    FILE *fref, *fseq;
+    char op, message[SIZE] = {0};
+    char *file = NULL;
+    int fSize;
+
     while((op = menu()) != '2') {
         if(op == '0') {
-            strcpy(message, "0");
+            // Open seq file
+            if((fseq = fopen("seq.txt", "r")) == NULL) {
+                perror("Error opening the file");
+                exit(EXIT_FAILURE);
+            }
 
+            // Count seq file length
+            fSize = getFileSize(fseq);
+
+            // Allocate buffer to store file
+            file = malloc(sizeof(char) * (fSize + 2)); // Add 2 (1 for option, 1 for null)
+            size_t newLen = fread(file, sizeof(char), fSize, fseq);
+            if ( ferror( fseq ) != 0 ) {
+                perror("Error reading file");
+                exit(EXIT_FAILURE);
+            } else {
+                file[newLen++] = '0'; // Adds option
+                file[newLen++] = '\0'; // Add NULL terminator
+            }
+
+            // Send length to server
+            sprintf(message, "%zu", newLen);
+            printf("%s\n", message);
+            if(send(sockfd, message, SIZE, 0) == -1) {
+                perror("Error sending message length");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("%zu\n", newLen);
+            printf("Message tail: %s\n", file + newLen - 11); // Show last 10 chars of seq
+            // Send File
+            void *p = file;
+            int bytes_written = 0;
+            while (1)
+            {
+                bytes_written += write(sockfd, p, SIZE);
+                if(bytes_written >= fSize) {
+                    break;
+                }
+                p = file + bytes_written;
+            }
+
+            // (Optional) Receive OK message from server 
+            char status[3] = {0};
+            SocketReceive(sockfd, status, sizeof(status));
+            if(strcmp(status, "OK") == 0) printf("Upload Success\n");
+            fclose(fseq);
+            free(file);
         } else if(op == '1') {
-            strcpy(message, "1");
+            // strcpy(message, "1");
         } else {
             printf("Please enter a valid option");
             continue;
         }
-        // write(sockfd, message, sizeof(message));
-        if((fd = open("reference.txt", O_RDONLY)) == -1) {
-            perror("[-]Error opening file");
-            exit(EXIT_FAILURE);
-        }
-        sendFile(sockfd, fd);
-        printf("File copied successfully\n");
-        close(fd);
+        // close(fd);
     }
 
     close(sockfd);
     printf("[+]Connection closed.\n");
-
     return 0;
 }
--- server.c
+++ server.c
@@ -8,15 +8,13 @@
 #define SIZE 1024
 #define FILECHUNK 4096
 #define DEBUG 1
+#define PORT 8000
 
 enum _OPTIONS {
     SEQ,
     REF,
     EXIT
 };
-
-char *seqPath;
-char *refPath;
 
 void writeFile(int sockfd, int fd) {
     char buff[FILECHUNK];
@@ -32,6 +30,7 @@ void writeFile(int sockfd, int fd) {
     }
     return;
 }
+
 
 void readOption(int sockfd) {
     char buff[SIZE] = {0};
@@ -104,78 +103,186 @@ void setup(int sockfd) {
     // }
 }
 
+// int main()
+// {
+//     char *ip = "127.0.0.1";
+//     int port = 8080;
+//     int e;
+
+//     int serverfd, clientfd, readLength;
+//     struct sockaddr_in server_addr, new_addr;
+//     socklen_t addr_size;
+
+//     char clientMsg[SIZE] = {0};
+//     char serverMsg[SIZE] = {0};
+
+//     // Create Scoket File Descriptor
+//     serverfd = socket(AF_INET, SOCK_STREAM, 0);
+//     if (serverfd < 0)
+//     {
+//         perror("[-]Error in socket");
+//         exit(1);
+//     }
+//     printf("[+]Server socket created successfully.\n");
+
+//     //Forcefully attaching socket to the port 8080
+//     int opt = 1;
+//     if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR,
+//                    &opt, sizeof(opt)))
+//     {
+//         syslog(LOG_NOTICE, "[-]Error setting socket options");
+//         perror("[-]Error setting socket options");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     server_addr.sin_family = AF_INET;
+//     server_addr.sin_port = port;
+//     server_addr.sin_addr.s_addr = inet_addr(ip);
+
+//     // Bind Adress to Socket
+//     if (bind(serverfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+//     {
+//         perror("[-]Error in bind");
+//         exit(1);
+//     }
+//     printf("[+]Binding successfull.\n");
+
+//     // Listen
+//     if (listen(serverfd, 3) == 0)
+//     {
+//         printf("[+]Listening....\n");
+//     }
+//     else
+//     {
+//         perror("[-]Error in listening");
+//         exit(1);
+//     }
+
+//     addr_size = sizeof(new_addr);
+//     while((clientfd = accept(serverfd, (struct sockaddr *)&new_addr, &addr_size)) > 0) {
+//         printf("[+]Connection accepted.\n");
+//         // syslog(LOG_NOTICE, "Connection Accepted");
+//         printf("Hello world");
+//         // fflush(stdout);
+
+//         // Receives length of next message
+//         while((readLength = recv(clientfd, clientMsg, SIZE, 0)) > 0) {
+//             printf("Hello world");
+//             int nextMsgLength = atoi(clientMsg);
+//             bzero(clientMsg, SIZE);
+
+//             // Allocates mem based on next msg len
+//             char *buffer = (char*) malloc((sizeof(char) * nextMsgLength) + 2);
+//             bzero(buffer, nextMsgLength + 2);
+
+//             // Receives actual message
+//             recv(clientfd, buffer, nextMsgLength, 0);
+//             bzero(clientMsg, SIZE);
+
+//             // Print message tail
+//             printf("%s", buffer + nextMsgLength - 11);
+//         }
+
+//         close(clientfd);
+//         // setup(clientfd);
+
+//         // int fd = open("ref2.txt", O_WRONLY | O_CREAT);
+//         // writeFile(clientfd, fd);
+//         // write_file(clientfd);
+//         // printf("[+]Data written in the file successfully.\n");
+//         // printf("[+]Connection terminated.\n");
+//         // fflush(stdout);
+//     }
+//     return 0;
+// }
+
 int main()
 {
     char *ip = "127.0.0.1";
-    int port = 8080;
-    int e;
-
-    int serverfd, clientfd;
-    struct sockaddr_in server_addr, new_addr;
+    int serverfd, clientfd, readLength;
     socklen_t addr_size;
-    char buffer[SIZE];
+    struct sockaddr_in server, client;
+    char clientMsg[SIZE] = {0};
+    char serverMsg[SIZE] = {0};
+    char *buffer = NULL;
 
-    // Create Scoket File Descriptor
+    // Create socket fd
     serverfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverfd < 0)
+    if (serverfd == -1)
     {
-        perror("[-]Error in socket");
-        exit(1);
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
     }
-    printf("[+]Server socket created successfully.\n");
+    printf("Created Socket Successfully\n");
 
     //Forcefully attaching socket to the port 8080
     int opt = 1;
     if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR,
                    &opt, sizeof(opt)))
     {
-        syslog(LOG_NOTICE, "[-]Error setting socket options");
         perror("[-]Error setting socket options");
         exit(EXIT_FAILURE);
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = port;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server.sin_family = AF_INET;
+    server.sin_port = PORT;
+    server.sin_addr.s_addr = inet_addr(ip);
 
-    // Bind Adress to Socket
-    if (bind(serverfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    // Bind
+    if (bind(serverfd, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
-        perror("[-]Error in bind");
-        exit(1);
+        perror("Error in bind");
+        exit(EXIT_FAILURE);
     }
-    printf("[+]Binding successfull.\n");
+    printf("Bind Successfull\n");
 
-    while(1) {
-        // Listen
-        if (listen(serverfd, 1) == 0)
-        {
-            printf("[+]Listening....\n");
-        }
-        else
-        {
-            perror("[-]Error in listening");
-            exit(1);
-        }
-
-        addr_size = sizeof(new_addr);
-        if((clientfd = accept(serverfd, (struct sockaddr *)&new_addr, &addr_size)) < 0) {
-            perror("[-]Error in accept");
-            exit(EXIT_FAILURE);
-        }
-        #if DEBUG == 1
+    // Listen
+    if (listen(serverfd, 3) == 0)
+    {
+        printf("[+]Listening....\n");
+    }
+    else
+    {
+        perror("[-]Error in listening");
+        exit(EXIT_FAILURE);
+    }
+    addr_size = sizeof(struct sockaddr_in);
+    while ((clientfd = accept(serverfd, (struct sockaddr *)&client, (socklen_t *)&addr_size)))
+    {
         printf("[+]Connection accepted.\n");
-        #endif
-        syslog(LOG_NOTICE, "Connection Accepted");
+        // syslog(LOG_NOTICE, "Connection Accepted");
+        // printf("Hello world");
+        fflush(stdout);
 
-        // setup(clientfd);
+        // Receives length of next message
+        while ((readLength = recv(clientfd, clientMsg, sizeof(clientMsg), 0)) <= SIZE)
+        {
+            int nextMsgLength = atoi(clientMsg);
+            printf("%d\n", nextMsgLength);
 
-        int fd = open("ref2.txt", O_WRONLY | O_CREAT);
-        writeFile(clientfd, fd);
-        // write_file(clientfd);
-        printf("[+]Data written in the file successfully.\n");
-        printf("[+]Connection terminated.\n");
-        // fflush(stdout);
+            // Allocates mem based on next msg len
+            buffer = (char *) malloc(sizeof(char) * nextMsgLength);
+            bzero(buffer, nextMsgLength);
+
+            // Receive message in chunks
+            int n = 0;
+            int xRead;
+            while(n < nextMsgLength) {
+                xRead = read(clientfd, clientMsg, SIZE);
+                memcpy(buffer + n, clientMsg, xRead);
+                bzero(clientMsg, SIZE);
+                n += xRead;
+            }
+            // Print message tail
+            printf("Message tail: %s\n", buffer + strlen(buffer) - 10);
+            printf("%c\n", *(buffer + strlen(buffer) - 1));
+
+            send(clientfd, "OK", 3, 0);
+            bzero(clientMsg, SIZE);
+            free(buffer);
+        }
+        fflush(stdout);
+        close(clientfd);
     }
     return 0;
 }
